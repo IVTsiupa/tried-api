9f9d791240264563d1edfe008961a1a9644d0a23
Ivan Tsiupa <vattgert@gmail.com>
2022-04-03 15:22:52 +0300

    create basic DDD building blocks

---
+++ src/shared/domain/ValueObject.ts
@@ -0,0 +1,25 @@
+interface ValueObjectProps {
+  [index: string]: any;
+}
+
+export abstract class ValueObject<T extends ValueObjectProps> {
+  public properties: T;
+
+  constructor(properties: T) {
+    this.properties = {
+      ...properties,
+    };
+  }
+
+  public equals(other: ValueObject<T>): boolean {
+    if (other === null || other === undefined) {
+      return false;
+    }
+
+    if (other.properties === undefined) {
+      return false;
+    }
+
+    return JSON.stringify(this.properties) === JSON.stringify(other.properties);
+  }
+}
